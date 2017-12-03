7ac2b9264ba7796b8e4c17a96bd26282b3def3c6
Rafael Nanes <rafaelbraga_22@hotmail.com>
2017-12-03 10:03:29 -0200

    fixing the mock token

--- mocks/login-mock.js
+++ mocks/login-mock.js
@@ -2,7 +2,7 @@ var app = angular.module('myApp');
 
 app.run(function ($httpBackend) {
     $httpBackend.when('POST', 'http://localhost:8080/login')
-        .respond(200, { status: 0 }, { 'authorization': "eyJpZCI6MSwibG9naW4iOiJhZG1pbiIsInBlcm1pc3Npb25zIjpbInVzZXIuaW5zZXJ0IiwidXNlci5saXN0Il19.eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiaWF0IjoxNTEyMjMzMzg1LCJleHAiOjE1MTIyMzY5ODV9.BzPasCZMae_qoWU_FskRDsyRsqttWxgPIcQXmsrfz-k" })
+        .respond(200, { status: 0 }, { 'authorization': "eyJpZCI6MSwibG9naW4iOiJhZG1pbiIsInByb2ZpbGVJZCI6MSwicGVybWlzc2lvbnMiOlsidXNlci5vcGVyYXRpb24iLCJ1c2VyLmluZm8iXX0=||eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiaWF0IjoxNTEyMzAyNTY5LCJleHAiOjE1MTIzMDYxNjl9.qOJz4oeMd1q8P8_Gzd3nRTxuE0c6NhO7mEbCTiF-ozU" });
 });
 
 
